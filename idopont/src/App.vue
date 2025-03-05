59c4e6e09238f20f11230afa1423dfcda1cb38aa
2020b Bognár Kevin <98013419+b0BogKevin@users.noreply.github.com>
2025-03-05 14:04:17 +0100

    napválasztás

--- idopont/src/App.vue
+++ idopont/src/App.vue
@@ -1,19 +1,9 @@
 <script setup>
+import { RouterLink, RouterView } from 'vue-router'
 
 </script>
 
 <template>
-  <div class="content">
-    <h1>Pszichológus időpont foglalás</h1>
-    <div>Nap</div>
-    <select class="form-select form-select-lg">
-<option>Hétfő</option>
-<option>Kedd</option>
-<option>Szerda</option>
-<option>Csütörtök</option>
-<option>Péntek</option>
-
-      </select>
-  </div>
+  <RouterView/>
 </template>
 
---
+++ idopont/src/views/FoglalasView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+const nap = ref();
+const idopontok = ref();
+const vanNap = ref();
+const napSelect = ()=>{
+  
+vanNap.value=true;
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>Pszichológus időpont foglalás</h1>
+    <div>Nap</div>
+    <select v-model="nap" v-on:change="napSelect" class="form-select form-select-lg">
+<option value="1">Hétfő</option>
+<option value="2">Kedd</option>
+<option value="3">Szerda</option>
+<option value="4">Csütörtök</option>
+<option value="5">Péntek</option>
+
+      </select>
+
+<div v-if="vanNap"></div>
+<div v-else>Itt jelennek meg a lehetséges időpontok</div>
+
+  </div>
+
+</template>
+
