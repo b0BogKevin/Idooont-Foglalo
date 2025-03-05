3b95686b9d3c70e653224c24f6d7089830a917c1
2020b Bognár Kevin <98013419+b0BogKevin@users.noreply.github.com>
2025-03-05 13:57:01 +0100

    Kezdő állapot

---
+++ idopont/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="content">
+    <h1>Pszichológus időpont foglalás</h1>
+    <div>Nap</div>
+    <select class="form-select form-select-lg">
+<option>Hétfő</option>
+<option>Kedd</option>
+<option>Szerda</option>
+<option>Csütörtök</option>
+<option>Péntek</option>
+
+      </select>
+  </div>
+</template>
+
